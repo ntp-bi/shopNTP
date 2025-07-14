9e7fe6bc8e8c93af63d94195dfd54e0dbbf42b81
ntp-bi <bintp22122002@gmail.com>
2025-07-14 21:33:00 +0700

    Setup project

---
+++ BE/src/main/java/com/ntp/be/BeApplication.java
@@ -0,0 +1,14 @@
+package com.ntp.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
