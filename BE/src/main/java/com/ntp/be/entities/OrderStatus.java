c9052cc4689e7c0c01387430973770c7f5724198
ntp-bi <bintp22122002@gmail.com>
2025-07-17 20:51:01 +0700

    push entities

---
+++ BE/src/main/java/com/ntp/be/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.ntp.be.entities;
+
+public enum OrderStatus {
+    PENDING,
+    IN_PROGRESS,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ BE/src/main/java/com/ntp/be/entities/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.ntp.be.entities;
+
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
